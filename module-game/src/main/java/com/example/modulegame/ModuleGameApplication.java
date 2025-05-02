bac29c6f48a1af187d26a5309c2c80a4780e0cee
AirIHL <inhak1122@gmail.com>
2025-05-02 11:16:06 +0900

    🚨 Test: 게임 모듈 주석

--- module-game/src/main/java/com/example/modulegame/ModuleGameApplication.java
+++ module-game/src/main/java/com/example/modulegame/ModuleGameApplication.java
@@ -3,7 +3,7 @@ package com.example.modulegame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-// 테스트 주석22
+// 테스트 주석33
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ModuleGameApplication {
