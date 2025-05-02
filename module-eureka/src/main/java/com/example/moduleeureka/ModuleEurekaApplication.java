dd0b13977659b5396831dd3d4451bd6d3ae74b95
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-05-02 14:02:56 +0900

    🚨 Test: 배포 테스트

--- module-eureka/src/main/java/com/example/moduleeureka/ModuleEurekaApplication.java
+++ module-eureka/src/main/java/com/example/moduleeureka/ModuleEurekaApplication.java
@@ -3,7 +3,7 @@ package com.example.moduleeureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//sdfsd
+//sdfsdf
 @SpringBootApplication
 @EnableEurekaServer
 public class ModuleEurekaApplication {
--- module-game/src/main/java/com/example/modulegame/ModuleGameApplication.java
+++ module-game/src/main/java/com/example/modulegame/ModuleGameApplication.java
@@ -3,7 +3,7 @@ package com.example.modulegame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-// 테스트 주석669988446633essfgzxzcvsd
+// 테스트 주석669988446633essfgzxzcvsdsd
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ModuleGameApplication {
