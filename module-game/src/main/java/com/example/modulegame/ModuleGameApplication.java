aa4ce670ef3a0f1f6da1290298fa12bb18e74d12
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-05-02 12:10:44 +0900

    🚨 Test: 빌드 테스트

--- module-game/src/main/java/com/example/modulegame/ModuleGameApplication.java
+++ module-game/src/main/java/com/example/modulegame/ModuleGameApplication.java
@@ -3,7 +3,7 @@ package com.example.modulegame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-// 테스트 주석55667788
+// 테스트 주석5566778899
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ModuleGameApplication {
