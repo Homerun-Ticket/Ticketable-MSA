dcfef406dc20302e333b847eab4a7e2aa0ff0085
AirIHL <inhak1122@gmail.com>
2025-05-05 18:30:16 +0900

    🐛 Fix: gateway 의존성 변경 및 테스트

--- module-gateway/src/main/java/com/example/modulegateway/ModuleGatewayApplication.java
+++ module-gateway/src/main/java/com/example/modulegateway/ModuleGatewayApplication.java
@@ -3,7 +3,7 @@ package com.example.modulegateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-// 테스트 2
+// 테스트 3
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ModuleGatewayApplication {
